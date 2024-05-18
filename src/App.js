c85300168a084bc5d87ff646579bd26beb9f68f1
lonegunman82 <mateusz.pietrzyk@gmail.com>
2024-05-18 10:59:01 +0200

    Drugi comiit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react";
 function App() {
-    let email = 'mateusz.pietrzyk@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
 
     function handleChange(event) {
 
-        console.log(event.target.value);
-
-    }
-
+        setEmail(event.target.value);
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
             <h2>Twój e-mail to {email.toUpperCase()}</h2>
-            <input type="text" onChange={handleChange}/>
+            <input type="text" value={email} onChange={handleChange}/>
 
         </div>
     );
